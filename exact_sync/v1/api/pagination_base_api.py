0300de0c6544f8263acfa4983b57d1da1a2b9fd9
Christian Marzahl <christian.marzahl@gmail.com>
2020-04-23 17:29:32 +0200

    Added pagination based download

---
+++ exact_sync/v1/api/pagination_base_api.py
@@ -0,0 +1,36 @@
+from time import sleep
+import re
+
+class PaginationBaseAPI(object):
+    
+    def get_all(self, func:callable, **kwargs):
+
+        data = func(**kwargs)
+
+        if kwargs.get('async_req'):
+            data = data.get()
+        if data.next != None:   
+            threads = []             
+            limit = int(re.findall("limit=([0-9]*)", data.next)[0])
+            offset = int(re.findall("offset=([0-9]*)", data.next)[0])
+            for page_offset in range(offset, data.count, offset):
+                kwargs['offset'] = page_offset
+                if kwargs.get('async_req'):
+                    thread = func(**kwargs)
+                    threads.append(thread)
+                else:    
+                    (new_data) = func(**kwargs)
+                    data.results += new_data.results
+
+            if kwargs.get('async_req'):
+                while (len(threads) > 0):
+                    for thread in threads:
+                        if thread.ready():
+                            data.results += thread.get().results
+                            threads.remove(thread)
+                    sleep(0.25)
+
+            data.next = None
+            data.previous = None
+        return data
+
