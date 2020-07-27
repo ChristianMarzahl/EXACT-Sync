1705dac51391d027323e5212b09006220461ee75
Christian Marzahl <c.marzahl@euroimmun.de>
2020-07-27 13:52:55 +0200

    Added download thumbnail

--- exact_sync/v1/api/images_api.py
+++ exact_sync/v1/api/images_api.py
@@ -855,3 +855,67 @@ class ImagesApi(PaginationBaseAPI):
 
         return status_code, target_path
 
+    def download_thumbnail(self, id, target_path:Path, **kwargs):
+
+        all_params = ['id']  # noqa: E501
+        all_params.append('async_req')
+        all_params.append('_return_http_data_only')
+        all_params.append('_preload_content')
+        all_params.append('_request_timeout')
+
+        params = locals()
+        for key, val in six.iteritems(params['kwargs']):
+            if key not in all_params:
+                raise TypeError(
+                    "Got an unexpected keyword argument '%s'"
+                    " to method retrieve_image" % key
+                )
+            params[key] = val
+        del params['kwargs']
+        # verify the required parameter 'id' is set
+        if ('id' not in params or
+                params['id'] is None):
+            raise ValueError("Missing the required parameter `id` when calling `retrieve_image`")  # noqa: E501
+
+        collection_formats = {}
+        path_params = {}
+        header_params = {}
+
+        query_params = []
+        form_params = []
+        local_var_files = {}
+
+        body_params = None
+        # HTTP header `Accept`
+        header_params['Accept'] = self.api_client.select_header_accept(
+            ['application/json'])  # noqa: E501
+
+        # Authentication setting
+        auth_settings = ['basicAuth']  # noqa: E501
+
+        response, status_code, header = self.api_client.call_api(
+            '/api/v1/images/images/{}/thumbnail/'.format(id), 'GET',
+            path_params,
+            query_params,
+            header_params,
+            body=body_params,
+            post_params=form_params,
+            files=local_var_files,
+            response_type='Image',  # noqa: E501
+            auth_settings=auth_settings,
+            async_req=params.get('async_req'),
+            _return_http_data_only=params.get('_return_http_data_only'),
+            _preload_content=params.get('_preload_content', False),
+            _request_timeout=params.get('_request_timeout'),
+            collection_formats=collection_formats)
+
+
+        with response as r:
+            with open(str(target_path),'wb') as f:
+                chunk_size=8192
+                while True:
+                    data = r.read(chunk_size)
+                    if not data: break
+                    f.write(data)
+
+        return status_code, target_path
